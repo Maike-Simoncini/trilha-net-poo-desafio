8810ed71a4ac58066b2a56b392e755834cbbe73c
Maike Simoncini da Silva <maikesimoncinims@gmail.com>
2025-10-18 18:22:30 -0300

    Implement tests for Nokia and iPhone
    Added tests for Nokia and iPhone classes.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Testando o Nokia:");
+Nokia nokia = new Nokia("123456", "Modelo A", "IMEI123", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\nTestando o iPhone:");
+Iphone iphone = new Iphone("654321", "Modelo B", "IMEI456", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
